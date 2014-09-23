a13bdf06f8cb0b7ef9074d6f79a7863e139388b5
varvelworld <varvelworld@gmail.com>
2014-09-23 15:34:29 +0800

    add 1001.cpp add 1002.cpp

---
+++ 1001/1001.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <stack>
+
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    cin >> a >> b;
+    sum = a + b;
+    stack<int> stk;
+
+    if(sum == 0)
+    {
+        stk.push(0);
+    }
+    else if(sum < 0)
+    {
+        cout << "-";
+        sum = -sum;
+    }
+
+    while(sum != 0)
+    {
+        stk.push(sum % 1000);
+        sum /= 1000;
+    }
+    cout << stk.top();
+    stk.pop();
+    while(!stk.empty())
+    {
+        cout << ',' << setfill('0') << setw(3) << stk.top();
+        stk.pop();
+    }
+}
---
+++ 1002/1002.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <limits>
+#include <stdio.h>
+using namespace std;
+
+struct Node
+{
+    float c;
+    int e;
+    Node* next;
+};
+
+int main()
+{
+    int k;
+    Node* head = new Node();
+    int size = 0;
+    head->e = numeric_limits<double>::max();
+    head->next = head;
+    int e;
+    float c;
+
+    Node* p = head;
+    cin >> k;
+    for(int i = 0; i < k; i++)
+    {
+        cin >> e >> c; 
+        Node* np = new Node();
+        np->e = e;
+        np->c = c;
+        np->next = p->next;
+        p->next = np;
+        ++size;
+        p = np;
+    }
+
+    cin >> k;
+    p = head;
+    for(int i = 0; i < k; i++)
+    {
+        cin >> e >> c; 
+        while(p->next != head && p->next->e >= e)
+        {
+            p = p->next;
+        }
+        if(p->e == e)
+        {
+            p->c += c;
+        }
+        else
+        {
+            Node* np = new Node();
+            np->e = e;
+            np->c = c;
+            np->next = p->next;
+            p->next = np;
+            ++size;
+            p = np;
+        }
+    }
+
+    p = head;
+    while(p->next != head)
+    {
+        if(p->next->c == 0)
+        {
+            Node* d = p->next;
+            p->next = d->next;
+            delete d;
+            --size;
+        }
+        else
+        {
+            p = p->next;    
+        }
+    }
+
+    cout << size;
+    p = head->next;
+    while(p != head)
+    {
+        printf(" %d %.1lf", p->e, p->c);
+        p = p->next;
+    }
+    cout << '\n';
+    return 0;
+}
