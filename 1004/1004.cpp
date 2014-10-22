5313b9c3e4a5a2e68d0e33afc5d5804b7859770d
varvelworld <varvelworld@gmail.com>
2014-10-22 10:52:49 +0800

    add 1004

---
+++ 1004/1004.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include<vector>
+
+using namespace std;
+
+struct Node 
+{
+    int level;
+    int childCount;
+    vector<int> children;
+    Node(): level(0), childCount(0){}
+};
+
+void calculateLevel(Node tree[100], int id, int level)
+{
+    tree[id].level = level;
+    for(vector<int>::iterator iter = tree[id].children.begin(); iter != tree[id].children.end(); ++iter)
+    {
+        calculateLevel(tree, *iter, level + 1);
+    }
+    return;
+}
+
+int main()
+{
+    int n, m;
+    Node tree[100];                   //1至99
+    int leafCountByLevel[100] = {0};  //各个层级叶子节点统计
+    int maxLevel = 0;
+
+    cin >> n >> m;
+    for(int i = 0; i < m; ++i)
+    {
+        int id, k;
+        cin >> id >> k;
+        tree[id].childCount = k;
+        for(int j = 0; j < k; ++j)
+        {
+            int childId;
+            cin >> childId;
+            tree[id].children.push_back(childId);
+        }
+    }
+    calculateLevel(tree, 1, 0);
+    for(int i = 1; i <= n; ++i)
+    {
+        if(tree[i].childCount == 0)
+        {
+            int level = tree[i].level;
+            ++(leafCountByLevel[level]); 
+            if(level > maxLevel) maxLevel = level;
+        }
+    }
+    for(int i = 0; i <= maxLevel; ++i)
+    {
+        cout << (i > 0 ? " " : "") << leafCountByLevel[i];
+    }
+    return 0;
+}
