d3b6b5446c19002e35c202514e9e1c7445e3736d
varvelworld <varvelworld@gmail.com>
2014-10-22 12:36:47 +0800

    add 1005

---
+++ 1005/1005.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string word[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+int main()
+{
+    string s;
+    long sum = 0;
+    cin >> s;
+    for(int i = 0; i < s.length(); ++i)
+    {
+        sum += s[i] - '0';
+    }
+    stack<string> spell;
+    while(sum != 0)
+    {
+        spell.push(word[sum % 10]);
+        sum /= 10;
+    }
+    if(!spell.empty())
+    {
+        cout << spell.top();
+        spell.pop();
+    }
+    else
+    {
+        cout << word[0];
+    }
+    while(!spell.empty())
+    {
+        cout << " " << spell.top();
+        spell.pop();
+    }
+    return 0;
+}
