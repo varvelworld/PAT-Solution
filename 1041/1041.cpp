0e4c2df62a05e8d127e1bc67e90ad2bee9501e41
varvelworld <varvelworld@gmail.com>
2015-01-17 16:50:43 +0800

    add 1041

---
+++ 1041/1041.cpp
@@ -0,0 +1,29 @@
+﻿#include <cstdio>
+#include <cstdlib>
+
+int c[10001];
+int n[100000];
+
+int main()
+{
+    int number_of_case;
+    scanf("%d", &number_of_case);
+    for(int i = 0; i < number_of_case; ++i)
+    {
+        int number;
+        scanf("%d", &number);
+        ++c[number];
+        n[i] = number;
+    }
+    int i;
+    for(i = 0; i < number_of_case; ++i)
+    {
+        if(c[n[i]] == 1)
+        {
+            printf("%d\n", n[i]);
+            break;
+        }
+    }
+    if(i == number_of_case) printf("None\n");
+    return 0;
+}
