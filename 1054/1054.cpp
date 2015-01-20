044b657864ab2b8cb2783762e0e8c4949bd6069b
varvelworld <varvelworld@gmail.com>
2015-01-20 12:15:50 +0800

    add 1054

---
+++ 1054/1054.cpp
@@ -0,0 +1,20 @@
+﻿#include <cstdio>
+#include <cstdlib>
+
+int main()
+{
+    int m, n;
+    scanf("%d%d", &m, &n);
+    int dominants_color = 0;
+    for(int i = 0; i < m * n; ++i)
+    {
+        int color;
+        scanf("%d", &color);
+        if(dominants_color == 0) 
+            dominants_color = color;
+        else if(color != dominants_color)
+            dominants_color = 0;
+    }
+    printf("%d\n", dominants_color);
+    return 0;
+}
