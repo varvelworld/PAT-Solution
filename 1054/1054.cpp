0b60d66b4504b3acc4db8af231723b728bad2f4f
varvelworld <varvelworld@gmail.com>
2015-01-21 10:10:16 +0800

    modify 1054

--- 1054/1054.cpp
+++ 1054/1054.cpp
@@ -6,14 +6,20 @@ int main()
     int m, n;
     scanf("%d%d", &m, &n);
     int dominants_color = 0;
+    int count_dominants_color = 0;
     for(int i = 0; i < m * n; ++i)
     {
         int color;
         scanf("%d", &color);
-        if(dominants_color == 0) 
+        if(count_dominants_color == 0) 
+        {
             dominants_color = color;
-        else if(color != dominants_color)
-            dominants_color = 0;
+            ++count_dominants_color;
+        }
+        else if(color == dominants_color)
+            ++count_dominants_color;
+        else
+            --count_dominants_color;
     }
     printf("%d\n", dominants_color);
     return 0;
